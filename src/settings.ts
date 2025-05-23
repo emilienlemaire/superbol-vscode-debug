fc6fcb43515a296e413b7d17d743aa1854e047e0
engboris <boris.eng@ocamlpro.com>
2025-05-23 17:31:21 +0200

    Make configs CamelCase

--- src/settings.ts
+++ src/settings.ts
@@ -4,20 +4,20 @@ export class DebuggerSettings {
     private readonly debugSettings: vscode.WorkspaceConfiguration;
 
     constructor() {
-	this.debugSettings = vscode.workspace.getConfiguration("superbol.debugger");
-	//this.globalSettings = vscode.workspace.getConfiguration("superbol");
+        this.debugSettings = vscode.workspace.getConfiguration("superbol.debugger");
+        //this.globalSettings = vscode.workspace.getConfiguration("superbol");
     }
 
     public get displayVariableAttributes(): boolean {
-        return this.debugSettings.get<boolean>("display-variable-attributes");
+        return this.debugSettings.get<boolean>("displayVariableAttributes");
     }
 
     public get gdbpath(): string {
-        return this.debugSettings.get<string>("gdb-path");
+        return this.debugSettings.get<string>("gdbPath");
     }
 
     public get libcobpath(): string {
-        return this.debugSettings.get<string>("libcob-path");
+        return this.debugSettings.get<string>("libcobPath");
     }
 
     public get gdbtty(): string {
